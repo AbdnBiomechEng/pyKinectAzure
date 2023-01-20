05a2b1ef1da0ad0387b6082c50eec9b3524dac2f
Bradley Scott <53945240+bs97@users.noreply.github.com>
2023-01-20 16:54:56 +0000

    Add more body tracking code

--- examples/exampleBodytrackingPlayback2trc.py
+++ examples/exampleBodytrackingPlayback2trc.py
@@ -39,37 +39,33 @@ def writeTRC(data, file):
 			file.write("\t%f\t%f\t%f" % tuple(data["Data"][i][l]))
 		file.write("\n")
 
-
 if __name__ == "__main__":
-	time.sleep(3)
+
+	video_filename = "test.mkv"
+
 	# Initialize the library, if the library is not found, add the library path as argument
 	pykinect.initialize_libraries(track_body=True)
 
-	# Modify camera configuration
-	device_config = pykinect.default_configuration
-	device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_1080P
-	device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
-	device_config.synchronized_images_only = True
+	# Start playback
+	playback = pykinect.start_playback(video_filename)
 
-	device_config.wired_sync_mode = 1
+	playback_config = playback.get_record_configuration()
+	# print(playback_config)
 
-	print(device_config)
-
-	# Start device
-	device = pykinect.start_device(config=device_config)
+	playback_calibration = playback.get_calibration()
 
 	# Start body tracker
-	bodyTracker = pykinect.start_body_tracker()
+	bodyTracker = pykinect.start_body_tracker(calibration=playback_calibration)
 
 	cv2.namedWindow('Depth image with skeleton',cv2.WINDOW_NORMAL)
+
 	runtimeseconds = 9
 	fps = 0
 	joints = np.zeros(shape=(0, 18, 3))
 	# joints = np.zeros(shape=(0, 32, 3))
 	endTime = datetime.datetime.now() + datetime.timedelta(seconds=runtimeseconds)
 	skips = 0
-
-
+	timestamps = []
 	import numpy as np
 
 	data = {'DataRate': 20,
@@ -80,71 +76,68 @@ if __name__ == "__main__":
 			'OrigDataStartFrame': 1,
 			'OrigNumFrames': 1,
 			'Labels': [
-					   'K4ABT_JOINT_PELVIS',
-					   'K4ABT_JOINT_SPINE_NAVEL',
-					   'K4ABT_JOINT_SPINE_CHEST',
-					   'K4ABT_JOINT_NECK',
-					   'K4ABT_JOINT_CLAVICLE_LEFT',
-					   'K4ABT_JOINT_SHOULDER_LEFT',
-					   'K4ABT_JOINT_ELBOW_LEFT',
-					   'K4ABT_JOINT_WRIST_LEFT',
-					   'K4ABT_JOINT_HAND_LEFT',
-					   'K4ABT_JOINT_HANDTIP_LEFT',
-					   'K4ABT_JOINT_THUMB_LEFT',
-					   'K4ABT_JOINT_CLAVICLE_RIGHT',
-					   'K4ABT_JOINT_SHOULDER_RIGHT',
-					   'K4ABT_JOINT_ELBOW_RIGHT',
-					   'K4ABT_JOINT_WRIST_RIGHT',
-					   'K4ABT_JOINT_HAND_RIGHT',
-					   'K4ABT_JOINT_HANDTIP_RIGHT',
-					   'K4ABT_JOINT_THUMB_RIGHT',
-					   # 'K4ABT_JOINT_HIP_LEFT',
-					   # 'K4ABT_JOINT_KNEE_LEFT',
-					   # 'K4ABT_JOINT_ANKLE_LEFT',
-					   # 'K4ABT_JOINT_FOOT_LEFT',
-					   # 'K4ABT_JOINT_HIP_RIGHT',
-					   # 'K4ABT_JOINT_KNEE_RIGHT',
-					   # 'K4ABT_JOINT_ANKLE_RIGHT',
-					   # 'K4ABT_JOINT_FOOT_RIGHT',
-					   # 'K4ABT_JOINT_HEAD',
-					   # 'K4ABT_JOINT_NOSE',
-					   # 'K4ABT_JOINT_EYE_LEFT',
-					   # 'K4ABT_JOINT_EAR_LEFT',
-					   # 'K4ABT_JOINT_EYE_RIGHT',
-					   # 'K4ABT_JOINT_EAR_RIGHT'
-					   ],
+				'K4ABT_JOINT_PELVIS',
+				'K4ABT_JOINT_SPINE_NAVEL',
+				'K4ABT_JOINT_SPINE_CHEST',
+				'K4ABT_JOINT_NECK',
+				'K4ABT_JOINT_CLAVICLE_LEFT',
+				'K4ABT_JOINT_SHOULDER_LEFT',
+				'K4ABT_JOINT_ELBOW_LEFT',
+				'K4ABT_JOINT_WRIST_LEFT',
+				'K4ABT_JOINT_HAND_LEFT',
+				'K4ABT_JOINT_HANDTIP_LEFT',
+				'K4ABT_JOINT_THUMB_LEFT',
+				'K4ABT_JOINT_CLAVICLE_RIGHT',
+				'K4ABT_JOINT_SHOULDER_RIGHT',
+				'K4ABT_JOINT_ELBOW_RIGHT',
+				'K4ABT_JOINT_WRIST_RIGHT',
+				'K4ABT_JOINT_HAND_RIGHT',
+				'K4ABT_JOINT_HANDTIP_RIGHT',
+				'K4ABT_JOINT_THUMB_RIGHT',
+				# 'K4ABT_JOINT_HIP_LEFT',
+				# 'K4ABT_JOINT_KNEE_LEFT',
+				# 'K4ABT_JOINT_ANKLE_LEFT',
+				# 'K4ABT_JOINT_FOOT_LEFT',
+				# 'K4ABT_JOINT_HIP_RIGHT',
+				# 'K4ABT_JOINT_KNEE_RIGHT',
+				# 'K4ABT_JOINT_ANKLE_RIGHT',
+				# 'K4ABT_JOINT_FOOT_RIGHT',
+				# 'K4ABT_JOINT_HEAD',
+				# 'K4ABT_JOINT_NOSE',
+				# 'K4ABT_JOINT_EYE_LEFT',
+				# 'K4ABT_JOINT_EAR_LEFT',
+				# 'K4ABT_JOINT_EYE_RIGHT',
+				# 'K4ABT_JOINT_EAR_RIGHT'
+			],
 			'Data': [],
 			'Timestamps': []
 			}
 
-	while fps < (runtimeseconds * 30):
-		# if datetime.datetime.now() >= endTime:
-		# 	break
-		# Get capture
-		capture = device.update()
+	while playback.isOpened():
+
+		# Get camera capture
+		capture = playback.update()
 
 		# Get body tracker frame
-		body_frame = bodyTracker.update()
+		body_frame = bodyTracker.update(capture=capture)
 
-		# Get the color depth image from the capture
+		# Get the colored depth
 		ret, depth_color_image = capture.get_colored_depth_image()
 
 		# Get the colored body segmentation
-		ret2, body_image_color = body_frame.get_segmentation_image()
+		ret, body_image_color = body_frame.get_segmentation_image()
 
 		if not ret:
-			skips = skips + 1
 			continue
 
+		# testing depth
 
-
-
-		# with open("C:/Users/bradl/Documents/GitHub/occlusion-study/calibration/testPrettyPrint1.json", "w") as write_file:
-		# 	json.dump(body_frame.json(), write_file, indent=4)
-		# 	print("json in file")
 		try:
 			bodies = body_frame.get_bodies()[0].numpy()
-
+			# get timestamp in usec
+			fTimestamp = body_frame.get_device_timestamp_usec()
+			timestamps.append(fTimestamp)
+			# print(fTimestamp)
 
 			fps = fps + 1
 			bodies = np.delete(bodies, np.s_[18:], axis=0)
@@ -176,9 +169,22 @@ if __name__ == "__main__":
 
 
 
+
+		 # end testing depth
+			
+		# Combine both images
+		combined_image = cv2.addWeighted(depth_color_image, 0.6, body_image_color, 0.4, 0)
+
+		# Draw the skeletons
+		combined_image = body_frame.draw_bodies(combined_image)
+
+		# Overlay body segmentation on depth image
+		cv2.imshow('Depth image with skeleton',combined_image)
+
 		# Press q key to stop
-		if cv2.waitKey(1) == ord('q'):
+		if cv2.waitKey(1) == ord('q'):  
 			break
+
 	cv2.destroyAllWindows()
 
 	print("joints shape before")
@@ -186,6 +192,7 @@ if __name__ == "__main__":
 
 	print("before joints row")
 	import c3d
+
 	writer = c3d.Writer()
 
 	for row in joints:
@@ -202,12 +209,12 @@ if __name__ == "__main__":
 	print(f"fps {fps}")
 	print(f"skips {skips}")
 
-	timestamps = list(range(0, fps))
+	# timestamps = list(range(0, fps))
 
 	data['Timestamps'] = timestamps
-	data['DataRate'] = fps/runtimeseconds
-	data['CameraRate'] = fps/runtimeseconds
-	data['OrigDataRate'] = fps/runtimeseconds
+	data['DataRate'] = fps / runtimeseconds
+	data['CameraRate'] = fps / runtimeseconds
+	data['OrigDataRate'] = fps / runtimeseconds
 	# data['NumMarkers']= 27
 	data['NumFrames'] = fps
 
@@ -224,12 +231,6 @@ if __name__ == "__main__":
 	# Write the data into the TRC file
 	writeTRC(data, outputfile)
 
-
-
 	# Clean up
 	outputfile.close()
 	print("fin")
-
-
-	# use frames for time not clock time as this will depend on processor speed
-	quit()
